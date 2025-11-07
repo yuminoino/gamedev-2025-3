67ec71759670ae61cb5bc7a2b3d9b813ba956b67
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-11-07 09:50:21 +0100

    jump

--- gamedev 3/Assets/Scripts/PlayerController.cs
+++ gamedev 3/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Start()
     {
         playerRb=GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
