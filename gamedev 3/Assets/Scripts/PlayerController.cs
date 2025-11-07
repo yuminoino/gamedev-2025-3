0673b09679ffa236f06005cc1f6c176161d98801
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-11-07 11:31:47 +0100

    jump obstacles

--- gamedev 3/Assets/Scripts/PlayerController.cs
+++ gamedev 3/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,46 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Rigidbody playerRb;
-
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerRb=GetComponent<Rigidbody>();
-       
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+
+
+
+            void OnCollisionEnter(Collision collision)
+            {
+                if (collision.gameObject.CompareTag("Obstacle"))
+                {
+                    Debug.Log("Hai colpito un ostacolo!");
+                }
+            }
+
         }
     }
-}
+
+    private void OnCollisionEnter(Collision collision)
+        { isOnGround = true;
+            }
+    }
+
+
+
---
+++ gamedev 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
